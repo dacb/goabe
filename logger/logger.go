f39deabbbac17a6d05fe4dbe87d8334e8176bd37
Dave Beck <dacb@uw.edu>
2024-01-02 00:48:13 -0800

    cleaned up comments for logging system

--- logger/logger.go
+++ logger/logger.go
@@ -8,8 +8,18 @@ import (
 	"github.com/spf13/viper"
 )
 
+// This is the system wide log interface.
 var Log *slog.Logger
 
+// InitLogger reads configuration to initialize logging for goabe.
+// The function needs the viper configuration system to be available
+// which also needs the cobra command line parser to be availabel.
+// These two dependencies means this function exists instead of doing
+// this with init().
+// When this function is run, the configuration will already have been
+// read from whatever file specified by the command line arguments.
+// This tooling runs dual logging streams: one on the stdout for text
+// based logging and the other on the json file.
 func InitLogger() {
 	// initialize the system using the config data from viper
 	logfile, err := os.OpenFile(viper.GetString("log_file"), os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0666)
